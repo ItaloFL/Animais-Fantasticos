180be52cb0e8fa24f9e356ad86ce4f6a862f23e8
Italo Ferreira <grd788@gmail.com>
2022-02-18 00:53:51 -0300

    :wrench: config webpack and babel

--- js/modules/fetchBtc.js
+++ js/modules/fetchBtc.js
@@ -1,14 +1,14 @@
-export default function fetchBtc() {
+export default async function fetchBtc() {
+  try {
+    const liBtc = document.querySelector('.btc-preco')
+    const valorBtc = await fetch('https://blockchain.info/ticker');
+    const JsonValor = await valorBtc.json();
+  
+    const valor = (JsonValor.BRL.sell/10000).toFixed(2)
+  
+    liBtc.innerText = `R$ ${valor}`;
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-try {
-  const liBtc = document.querySelector('.btc-preco')
-  const valorBtc = await fetch('https://blockchain.info/ticker');
-  const JsonValor = await valorBtc.json();
-
-  const valor = (JsonValor.BRL.sell/10000).toFixed(2)
-
-  liBtc.innerText = `R$ ${valor}`;
-} catch (error) {
-  console.log(error)
-}
